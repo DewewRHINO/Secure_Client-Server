98de3e8ffbddbaff9a764d543fb3c89aec13c185
luisaruiz0 <98635984+luisaruiz0@users.noreply.github.com>
2024-04-06 08:20:19 -0700

    added receive messages method

--- client.py
+++ client.py
@@ -3,7 +3,15 @@ import ssl
 import sys
 import threading
 
-
-
-
-
+def receive_messages(secure_socket):
+    while True:
+        try:
+            message = secure_socket.recv(2048)
+            if message:
+                print(message.decode('utf-8'))
+            else:
+                raise Exception("Server closed the connection")
+        except Exception as e:
+            print(f"Disconnected from server: {e}")
+            secure_socket.close()
+            sys.exit()
