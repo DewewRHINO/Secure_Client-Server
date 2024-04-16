ab6dcf57a53876d13aa9650730e993c03a7acbc8
lancesahagun <lance.sahagun03@gmail.com>
2024-04-16 13:50:56 -0700

    broadcast message and client threading
    send a broadcast message from one client to all other connected clients minus the server
    
    remove client from list of clients and close sockets
    
    communication handling between each client through client threading

--- server.py
+++ server.py
@@ -2,7 +2,34 @@ import socket
 import ssl
 import threading
 
+def broadcast_messages(client_message, all_clients, sender_socket):
+    for client_socket in all_clients:
+        if client_socket is not sender_socket:
+            try:
+                client_socket.send(client_message)
+            except Exception as e:
+                print(f"Error broadcasting message: {e}")
+                close_client_connection(client_socket, all_clients)
 
+def close_client_connection(client_socket, all_clients):
+    if client_socket in all_clients:
+        all_clients.remove(client_socket)
+    client_socket.close()
+
+def client_thread(client_socket, all_clients):
+    try:
+        client_socket.send(b'Welcome to the chat room!\n')
+        while True:
+            message = client_socket.recv(2048)
+            if message:
+                print(f"Broadcasting message: {message.decode('utf-8').strip()}")
+                broadcast_messages(message, all_clients, client_socket)
+            else:
+                raise Exception("Client disconnected")
+    except Exception as e:
+        print(f"Client disconnected: {e}")
+    finally:
+        close_client_connection(client_socket, all_clients)
 
 
 
